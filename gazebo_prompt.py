ff92820f1c911237bc02bb63bbe05dcdc84ad9d3
ZeddBao <baozeyubzy@gmail.com>
2023-12-29 05:53:04 +0800

    update

--- gazebo_prompt.py
+++ gazebo_prompt.py
@@ -3,7 +3,6 @@ gazebo_prompt = [{'role': 'system', 'content': 'Input: Query\nOutput: Pure Pytho
         '''在 (1, 1, 1) 处放置一个大小为 0.2 方块。'''},
     {'role': 'assistant', 'content':
         '''self.spawn_cube(position=[1, 1, 1], model_name='cube1', model_size=0.2, model_mass=0.1)'''},
-        '''self.spawn_cube(position=[1, 1, 1], model_name='cube1', model_size=0.2, model_mass=0.1)'''},
     {'role': 'user', 'content':
         '''在随机位置放置大小随机的5个方块。'''},
     {'role': 'assistant', 'content':
@@ -12,5 +11,5 @@ gazebo_prompt = [{'role': 'system', 'content': 'Input: Query\nOutput: Pure Pytho
 for i in range(5):
     position = [random.uniform(-5, 5), random.uniform(-5, 5), random.uniform(0, 5)]
     size = random.uniform(0.1, 0.5)
-    self.spawn_cube(position=position, model_name='cube'+str(i), model_size=size, model_mass=0.1)'''},
+    self.spawn_cube(position=position, model_name='cube'+str(i), model_size=size, model_mass=0.1)'''}
 ]
